6921a4c42f0d3d55ef1ddec65228481a89a957e5
DenMitter <57758gfk@gmail.com>
2024-11-14 10:48:00 +0200

    Add admin middleware and role system

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
-use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
---
+++ app/Services/Admin/UserService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services\Admin;
+
+use App\Models\User;
+use Exception;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Str;
+
+class UserService
+{
+    public function index()
+    {
+
+    }
+}
