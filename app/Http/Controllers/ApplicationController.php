71121ba3d621d04a8da7da1b802d1449e1994ca5
DenMitter <57758gfk@gmail.com>
2025-04-09 19:34:59 +0300

    feat: send anket to google sheets from landing

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Applications\StoreRequest;
 use App\Models\Applications;
+use App\Services\GoogleSheetsService;
 use Exception;
 use Illuminate\Http\JsonResponse;
 
 class ApplicationController extends Controller
 {
+    protected GoogleSheetsService $googleSheetsService;
+
+    public function __construct(GoogleSheetsService $googleSheetsService)
+    {
+        $this->googleSheetsService = $googleSheetsService;
+    }
+
     /**
      * Отримуємо список заявок.
      *
@@ -38,9 +46,26 @@ class ApplicationController extends Controller
     public function store(StoreRequest $request): JsonResponse
     {
         try {
+            $data = $request->validated();
+            $application = Applications::query()->create($data);
+
+            $this->googleSheetsService->addDataToSheet(
+                [
+                    $application->id,
+                    $application->name,
+                    $application->email,
+                    'Новий учень',
+                    'Лендінг',
+                    now()->toDateTimeString(),
+                    strtoupper($application->level),
+                ],
+                '1QBQO3a3AvZAJxMJVviZGoqVmChL-G9eQDr9nRiwKT9M',
+                'Заявки'
+            );
+
             return response()->json([
                 'success' => true,
-                'data' => Applications::query()->create($request->validated()),
+                'data' => $application->makeHidden(['updated_at']),
             ]);
         } catch (Exception $exception) {
             return response()->json([
