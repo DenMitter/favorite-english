a64e8b6d41ec7fae26c4d640f2a0bdac00810db7
Богдан Жижула <zizulabogdan@gmail.com>
2025-04-22 21:59:15 +0300

    feat: fix back

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,36 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import os from 'os';
+
+function getLocalIP() {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]) {
+            if (iface.family === 'IPv4' && !iface.internal) {
+                return iface.address;
+            }
+        }
+    }
+    return '127.0.0.1';
+}
+
+const localIP = getLocalIP();
+const isProdNetwork = localIP === '91.238.103.125'; // заміни на свій IP
 
 export default defineConfig({
     server: {
         host: '0.0.0.0',
         port: 5173,
+        https: {
+            key: fs.readFileSync('./ssl/key.pem'),
+            cert: fs.readFileSync('./ssl/cert.pem'),
+        },
         watch: {
             usePolling: true,
         },
         hmr: {
-            host: 'localhost',
+            host: isProdNetwork ? 'favorite-english.com' : 'localhost',
             protocol: 'ws',
         },
     },
